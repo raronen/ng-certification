87939ef4fcce5507599856a17e4f9c23735530df
Raz Ronen <raronen@microsoft.com>
2021-01-08 00:38:16 +0200

    delete unnecessarry file

--- src/app/common/displayDate.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'image' })
-export class ImagePipe implements PipeTransform {
-    public transform(day: number): string {
-        
-        return `https://www.angulartraining.com/images/weather/${image}.png`;
-    }
-}
